d4ac30fe6187d2726eb77f756559bd5c7b69613c
Jean Loberg Hansen <jeanloberg@gmail.com>
2023-11-14 21:44:28 +0100

    add topbar and city info.

---
+++ components/atoms/cityInfo/cityInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span>Copenhagen, Denmark</span>. <span>{{ time }}</span>
+  </div>
+</template>
+<script lang="ts" setup>
+const time = ref<string>();
+const setTime = () => {
+  // create a new Date object for the current time
+  const now = new Date();
+  // set the timezone to Copenhagen's timezone
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "Europe/Copenhagen",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  };
+
+  // format the time in the desired format
+  const timeInCopenhagen = new Intl.DateTimeFormat("en-DK", options).format(
+    now
+  );
+
+  time.value = timeInCopenhagen;
+};
+onMounted(() => {
+  setTime();
+  // update every 60 minutes
+  setInterval(() => {
+    setTime();
+  }, 60000);
+});
+</script>
+<style src="./cityInfo.scss" scoped />
--- pages/about.vue
+++ pages/about.vue
@@ -1,36 +1,7 @@
 <template>
   <div>
     <div>Page: about</div>
-    <div>
-      <button @click="execute()">Get data</button>
-    </div>
-    <h2>{{ openMeteoStatus }}</h2>
-
-    <div v-if="openMeteoPending && openMeteoStatus !== 'idle'">
-      Loading comments...
-    </div>
-
-    <div v-else-if="openMeteoStatus === 'success'">
-      {{ openMeteoData }}
-    </div>
-
-    <div v-if="!openMeteoPending && openMeteoData">
-      useWeather: {{ openMeteoData }}
-
-      icon:
-      <lazy-atoms-weather-icon
-        v-if="openMeteoData.current_weather.weathercode"
-        :code="openMeteoData.current_weather.weathercode"
-        :is-day="openMeteoData.current_weather.is_day"
-      />
-    </div>
+    <organisms-topbar />
   </div>
 </template>
-<script lang="ts" setup>
-const {
-  data: openMeteoData,
-  pending: openMeteoPending,
-  status: openMeteoStatus,
-  execute,
-} = await useOpenMeteo();
-</script>
+<script lang="ts" setup></script>
